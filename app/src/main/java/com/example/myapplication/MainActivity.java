34d18bad79680fc4580078628c71c4572a6061a6
Ismael <maelleal@msn.com>
2019-06-02 15:49:24 -0300

    teste

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        Button button = (Button) findViewById(R.id.button);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                setContentView(R.layout.activity_jogos);
+//            }
+//        });
+
+    }
+
+    public void CliqueEntrar(View view){
+        EditText editText = (EditText) findViewById(R.id.editText);
+        EditText campoSenha = (EditText) findViewById(R.id.camposenha);
+        TextView textView = (TextView) findViewById(R.id.textView);
+
+        setContentView(R.layout.pagina_inicial);
+
+    }
+
+    public void proximoJogo(View view){
+
+    }
+
+    public void classificacao(View view){
+
+    }
+
+    public void ultimoResutado(View view){
+
+    }
+
+    public void localPartidas(View view){
+
+    }
+
+    public void sobreAEquipe(View view){
+
+    }
+}
